82b24e43d7bde2e6ac2dc6d69f4758855e167646
btbright <btbright@gmail.com>
2018-12-03 16:20:07 -0600

    Updates

---
+++ precache-manifest.cb592728f6cceabfbe2461826264bf0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "196376c6075230b1f49f",
+    "url": "/learn-a-bit/static/js/runtime~main.196376c6.js"
+  },
+  {
+    "revision": "34264de5b6a2e48729db",
+    "url": "/learn-a-bit/static/js/main.34264de5.chunk.js"
+  },
+  {
+    "revision": "e2492e37b25a96e87218",
+    "url": "/learn-a-bit/static/js/1.e2492e37.chunk.js"
+  },
+  {
+    "revision": "34264de5b6a2e48729db",
+    "url": "/learn-a-bit/static/css/main.d5f1627a.chunk.css"
+  },
+  {
+    "revision": "71966481368d079adef5a261ebdb6332",
+    "url": "/learn-a-bit/index.html"
+  }
+];
